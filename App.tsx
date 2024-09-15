f580651d8f78e5aabb91f29b1fcef65c0ec0c614
Anurag Pramanik <p.anurag@outlook.com>
2024-09-15 15:51:59 +0530

    Add set up vector icon package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
 const App = () => {
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
-      <Text className="text-2xl">Hello React Native</Text>
+      <Text className="text-2xl mb-5">Hello React Native</Text>
+      <Icon name="react" size={50} />
     </SafeAreaView>
   );
 };
